098ed14c2a25ea98b120f2911e731fd84cfb95ee
Cruz <janrussel.cruz@dxc.com>
2023-05-30 09:57:46 +0800

    Commit all changes

--- fpoc16/lib/widgets/tracking_confirmation_screen.dart
+++ fpoc16/lib/widgets/tracking_confirmation_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class TrackingScreen extends StatefulWidget {
-  const TrackingScreen({super.key});
+  const TrackingScreen({required Key key}) : super(key: key);
 
   @override
   State<TrackingScreen> createState() => _TrackingScreenState();
@@ -19,6 +19,7 @@ class _TrackingScreenState extends State<TrackingScreen> {
     updateCurrentMonth();
     super.initState();
   }
+
   @override
   void dispose() {
     _monthController.close();
@@ -70,103 +71,103 @@ class _TrackingScreenState extends State<TrackingScreen> {
       ),
     );
   }
-}
 
-Widget _buildBody() {
-  return SizedBox(
-    width: 400,
-    height: 300,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        SizedBox(
-          width: 400,
-          child: Card(
-            margin: const EdgeInsets.fromLTRB(7, 5, 7, 0),
-            elevation: 1.5,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+  Widget _buildBody() {
+    return SizedBox(
+      width: 400,
+      height: 300,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(
+            width: 400,
+            child: Card(
+              margin: const EdgeInsets.fromLTRB(7, 5, 7, 0),
+              elevation: 1.5,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(15.0, 15, 0, 0),
+                    child: Text(
+                      'Your weekly goals',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: StreamBuilder<String>(
+                      stream: monthStream,
+                      initialData: '',
+                      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                        String currentMonth = snapshot.data ?? '';
+                        return Text(
+                          'Month of $currentMonth',
+                          style: const TextStyle(
+                            fontSize: 10,
+                            color: Colors.grey,
+                          ),
+                          textAlign: TextAlign.start,
+                        );
+                      },
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      '1/4',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(20, 0, 0, 15),
+                    child: Text(
+                      'Achieved',
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(15.0, 15, 0, 0),
-                  child: Text(
-                    'Your weekly goals',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
-                    ),
-                    textAlign: TextAlign.start,
+                const Text(
+                  'NON-COMPLIANCE WEEK:',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.transparent,
+                    shadows: [Shadow(offset: Offset(0, -3), color: Color.fromARGB(255, 82, 82, 82))],
+                    decoration: TextDecoration.underline,
+                    decorationThickness: 2,
+                    decorationColor: Color.fromARGB(255, 82, 82, 82),
                   ),
+                  textAlign: TextAlign.start,
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20.0),
-                  child: StreamBuilder<String>(
-                    stream: monthStream,
-                    initialData: '',
-                    builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                      String currentMonth = snapshot.data ?? '';
-                      return Text(
-                        'Month of $currentMonth',
-                        style: const TextStyle(
-                          fontSize: 10,
-                          color: Colors.grey,
-                        ),
-                        textAlign: TextAlign.start,
-                      );
-                    },
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(left: 20.0),
-                  child: Text(
-                    '1/4',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                    ),
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(20, 0, 0, 15),
-                  child: Text(
-                    'Achieved',
-                    style: TextStyle(
-                      fontSize: 10,
-                      color: Colors.grey,
-                    ),
-                  ),
+                Image.asset(
+                  'assets/icons/logo1.png',
+                  height: 50,
+                  alignment: Alignment.topRight,
                 ),
               ],
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                'NON-COMPLIANCE WEEK:',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.transparent,
-                  shadows: [Shadow(offset: Offset(0, -3), color: Color.fromARGB(255, 82, 82, 82))],
-                  decoration: TextDecoration.underline,
-                  decorationThickness: 2,
-                  decorationColor: Color.fromARGB(255, 82, 82, 82),
-                ),
-                textAlign: TextAlign.start,
-              ),
-              Image.asset(
-                'assets/icons/logo1.png',
-                height: 50,
-                alignment: Alignment.topRight,
-              ),
-            ],
-          ),
-        ),
-      ],
-    ),
-  );
+        ],
+      ),
+    );
+  }
 }
